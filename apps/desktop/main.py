9189febcdc9e360d2a1114b1ed271f1d6fada120
Tom Mladenov <Tom.Mladenov@esa.int>
2019-12-11 17:31:21 +0100

    added try catch in case of error

--- apps/desktop/main.py
+++ apps/desktop/main.py
@@ -118,117 +118,146 @@ class Main(QMainWindow):
 
 
 	def update(self, csp, crc):
+		try:
+			csp_crc = csp.getCRC32C() #calculate the CRC of the CSP packet
+			sent_crc = int.from_bytes(crc, byteorder='big') #Check what was attached as a CRC
 
-		csp_crc = csp.getCRC32C()
-		sent_crc = int.from_bytes(crc, byteorder='big')
+			if csp_crc == sent_crc: #Compare
+				crc_ok = True
 
-		if csp_crc == sent_crc:
-			crc_ok = True
+				if csp.isBeacon():
+					temp_brd,\
+					temp_pa,\
+					last_rssi,\
+					last_rferr,\
+					tx_count,\
+					rx_count,\
+					tx_bytes,\
+					rx_bytes,\
+					active_conf,\
+					boot_count,\
+					boot_cause,\
+					last_contact,\
+					bgnd_rssi,\
+					tx_duty,\
+					tot_tx_count,\
+					tot_rx_count,\
+					tot_tx_bytes,\
+					tot_rx_bytes =  csp.getBeaconContents()
 
-			if csp.isBeacon():
-				temp_brd,\
-				temp_pa,\
-				last_rssi,\
-				last_rferr,\
-				tx_count,\
-				rx_count,\
-				tx_bytes,\
-				rx_bytes,\
-				active_conf,\
-				boot_count,\
-				boot_cause,\
-				last_contact,\
-				bgnd_rssi,\
-				tx_duty,\
-				tot_tx_count,\
-				tot_rx_count,\
-				tot_tx_bytes,\
-				tot_rx_bytes =  csp.getBeaconContents()
-
-				self.temp_brd_label.setText(str(temp_brd/10.0))
-				self.temp_pa_label.setText(str(temp_pa/10.0))
-				self.last_rssi_label.setText(str(last_rssi))
-				self.last_rferr_label.setText(str(last_rferr))
-				self.tx_count_label.setText(str(tx_count))
-				self.rx_count_label.setText(str(rx_count))
-				self.tx_bytes_label.setText(str(tx_bytes))
-				self.rx_bytes_label.setText(str(rx_bytes))
-				self.active_conf_label.setText(str(active_conf))
-				self.boot_count_label.setText(str(boot_count))
-				self.boot_cause_label.setText(str(boot_cause))
-				self.last_contact_label.setText(str(last_contact))
-				self.bgnd_rssi_label.setText(str(bgnd_rssi))
-				self.tx_duty_label.setText(str(tx_duty))
-				self.tot_tx_count_label.setText(str(tot_tx_count))
-				self.tot_rx_count_label.setText(str(tot_rx_count))
-				self.tot_tx_bytes_label.setText(str(tot_tx_bytes))
-				self.tot_rx_bytes_label.setText(str(tot_rx_bytes))
-
-
-				current_packet_count = self.packet_history_table.rowCount()
-				self.packet_history_table.insertRow(current_packet_count)
-				self.packet_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
-				self.packet_history_table.setItem(current_packet_count, 1, QTableWidgetItem('Beacon'))
-				self.packet_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
-				self.packet_history_table.setItem(current_packet_count, 3, QTableWidgetItem(str(crc_ok)))
-				self.packet_history_table.setItem(current_packet_count, 4, QTableWidgetItem(str(csp.priority)))
-				self.packet_history_table.setItem(current_packet_count, 5, QTableWidgetItem(NODES[str(csp.source)]))
-				self.packet_history_table.setItem(current_packet_count, 6, QTableWidgetItem(str(csp.destination)))
-				self.packet_history_table.setItem(current_packet_count, 7, QTableWidgetItem(str(csp.dest_port)))
-				self.packet_history_table.setItem(current_packet_count, 8, QTableWidgetItem(str(csp.source_port)))
-				self.packet_history_table.setItem(current_packet_count, 9, QTableWidgetItem(str(csp.flags)))
-
-				self.packet_history_table.item(current_packet_count, 3).setBackground(QtGui.QColor(0, 255, 0))
-				self.packet_history_table.scrollToBottom()
+					self.temp_brd_label.setText(str(temp_brd/10.0))
+					self.temp_pa_label.setText(str(temp_pa/10.0))
+					self.last_rssi_label.setText(str(last_rssi))
+					self.last_rferr_label.setText(str(last_rferr))
+					self.tx_count_label.setText(str(tx_count))
+					self.rx_count_label.setText(str(rx_count))
+					self.tx_bytes_label.setText(str(tx_bytes))
+					self.rx_bytes_label.setText(str(rx_bytes))
+					self.active_conf_label.setText(str(active_conf))
+					self.boot_count_label.setText(str(boot_count))
+					self.boot_cause_label.setText(str(boot_cause))
+					self.last_contact_label.setText(str(last_contact))
+					self.bgnd_rssi_label.setText(str(bgnd_rssi))
+					self.tx_duty_label.setText(str(tx_duty))
+					self.tot_tx_count_label.setText(str(tot_tx_count))
+					self.tot_rx_count_label.setText(str(tot_rx_count))
+					self.tot_tx_bytes_label.setText(str(tot_tx_bytes))
+					self.tot_rx_bytes_label.setText(str(tot_rx_bytes))
 
 
-				self.raw_history_table.insertRow(current_packet_count)
-				self.raw_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
-				self.raw_history_table.setItem(current_packet_count, 1, QTableWidgetItem(str(csp.getHex())))
-				self.raw_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
-				self.raw_history_table.scrollToBottom()
+					current_packet_count = self.packet_history_table.rowCount()
+					self.packet_history_table.insertRow(current_packet_count)
+					self.packet_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
+					self.packet_history_table.setItem(current_packet_count, 1, QTableWidgetItem('Beacon'))
+					self.packet_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
+					self.packet_history_table.setItem(current_packet_count, 3, QTableWidgetItem(str(crc_ok)))
+					self.packet_history_table.setItem(current_packet_count, 4, QTableWidgetItem(str(csp.priority)))
+					self.packet_history_table.setItem(current_packet_count, 5, QTableWidgetItem(NODES[str(csp.source)]))
+					self.packet_history_table.setItem(current_packet_count, 6, QTableWidgetItem(str(csp.destination)))
+					self.packet_history_table.setItem(current_packet_count, 7, QTableWidgetItem(str(csp.dest_port)))
+					self.packet_history_table.setItem(current_packet_count, 8, QTableWidgetItem(str(csp.source_port)))
+					self.packet_history_table.setItem(current_packet_count, 9, QTableWidgetItem(str(csp.flags)))
 
-				parsed_data = '{iso_time},{temp_brd},{temp_pa},{last_rssi},{last_rferr},{tx_count},{rx_count},{tx_bytes},{rx_bytes},{active_conf},{boot_count},{boot_cause},{last_contact},{bgnd_rssi},{tx_duty},{tot_tx_count},{tot_rx_count},{tot_tx_bytes},{tot_rx_bytes}'.format(iso_time=datetime.datetime.utcnow().isoformat(),\
-														temp_brd=temp_brd,\
-														temp_pa=temp_pa,\
-														last_rssi=last_rssi,\
-														last_rferr=last_rferr,\
-														tx_count=tx_count,\
-														rx_count=rx_count,\
-														tx_bytes=tx_bytes,\
-														rx_bytes=rx_bytes,\
-														active_conf=active_conf,\
-														boot_count=boot_count,\
-														boot_cause=boot_cause,\
-														last_contact=last_contact,\
-														bgnd_rssi=bgnd_rssi,\
-														tx_duty=tx_duty,\
-														tot_tx_count=tot_tx_count,\
-														tot_rx_count=tot_rx_count,\
-														tot_tx_bytes=tot_tx_bytes,\
-														tot_rx_bytes=tot_rx_bytes)
+					self.packet_history_table.item(current_packet_count, 3).setBackground(QtGui.QColor(0, 255, 0))
+					self.packet_history_table.scrollToBottom()
 
 
-				eventLogger.info('Received CSP beacon packet of length {LEN} bytes + 4 byte CRC32C check: OK'.format(LEN=csp.getLength()))
-				raw_data = str(csp.getHex()) + str(crc.hex())
-				rawLogger.info('{iso_time},{data}'.format(iso_time=datetime.datetime.utcnow().isoformat(), data=raw_data))
-				parsedBeaconLogger.info(parsed_data)
+					self.raw_history_table.insertRow(current_packet_count)
+					self.raw_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
+					self.raw_history_table.setItem(current_packet_count, 1, QTableWidgetItem(str(csp.getHex())))
+					self.raw_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
+					self.raw_history_table.scrollToBottom()
+
+					parsed_data = '{iso_time},{temp_brd},{temp_pa},{last_rssi},{last_rferr},{tx_count},{rx_count},{tx_bytes},{rx_bytes},{active_conf},{boot_count},{boot_cause},{last_contact},{bgnd_rssi},{tx_duty},{tot_tx_count},{tot_rx_count},{tot_tx_bytes},{tot_rx_bytes}'.format(iso_time=datetime.datetime.utcnow().isoformat(),\
+															temp_brd=temp_brd,\
+															temp_pa=temp_pa,\
+															last_rssi=last_rssi,\
+															last_rferr=last_rferr,\
+															tx_count=tx_count,\
+															rx_count=rx_count,\
+															tx_bytes=tx_bytes,\
+															rx_bytes=rx_bytes,\
+															active_conf=active_conf,\
+															boot_count=boot_count,\
+															boot_cause=boot_cause,\
+															last_contact=last_contact,\
+															bgnd_rssi=bgnd_rssi,\
+															tx_duty=tx_duty,\
+															tot_tx_count=tot_tx_count,\
+															tot_rx_count=tot_rx_count,\
+															tot_tx_bytes=tot_tx_bytes,\
+															tot_rx_bytes=tot_rx_bytes)
+
+
+					eventLogger.info('Received CSP beacon packet of length {LEN} bytes + 4 byte CRC32C check: OK'.format(LEN=csp.getLength()))
+					raw_data = str(csp.getHex()) + str(crc.hex())
+					rawLogger.info('{iso_time},{data}'.format(iso_time=datetime.datetime.utcnow().isoformat(), data=raw_data))
+					parsedBeaconLogger.info(parsed_data)
+
+				else:
+					current_packet_count = self.packet_history_table.rowCount()
+					self.packet_history_table.insertRow(current_packet_count)
+					self.packet_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
+					self.packet_history_table.setItem(current_packet_count, 1, QTableWidgetItem('SPP over CSP'))
+					self.packet_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
+					self.packet_history_table.setItem(current_packet_count, 3, QTableWidgetItem(str(crc_ok)))
+					self.packet_history_table.setItem(current_packet_count, 4, QTableWidgetItem(str(csp.priority)))
+					self.packet_history_table.setItem(current_packet_count, 5, QTableWidgetItem(NODES[str(csp.source)]))
+					self.packet_history_table.setItem(current_packet_count, 6, QTableWidgetItem(str(csp.destination)))
+					self.packet_history_table.setItem(current_packet_count, 7, QTableWidgetItem(str(csp.dest_port)))
+					self.packet_history_table.setItem(current_packet_count, 8, QTableWidgetItem(str(csp.source_port)))
+					self.packet_history_table.setItem(current_packet_count, 9, QTableWidgetItem(str(csp.flags)))
+
+					self.packet_history_table.item(current_packet_count, 3).setBackground(QtGui.QColor(0, 255, 0))
+					self.packet_history_table.scrollToBottom()
+
+					self.raw_history_table.insertRow(current_packet_count)
+					self.raw_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
+					self.raw_history_table.setItem(current_packet_count, 1, QTableWidgetItem(str(csp.getHex())))
+					self.raw_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
+					self.raw_history_table.scrollToBottom()
+
+					eventLogger.info('Received SPP over CSP packet of length {LEN} bytes + 4 byte CRC32C check: OK'.format(LEN=csp.getLength()))
+					data = str(csp.getHex()) + str(crc.hex())
+					rawLogger.info('{iso_time},{data}'.format(iso_time=datetime.datetime.utcnow().isoformat(), data=data))
 
 			else:
+				crc_ok = False
+
 				current_packet_count = self.packet_history_table.rowCount()
+
 				self.packet_history_table.insertRow(current_packet_count)
 				self.packet_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
-				self.packet_history_table.setItem(current_packet_count, 1, QTableWidgetItem('SPP over CSP'))
+				self.packet_history_table.setItem(current_packet_count, 1, QTableWidgetItem('--'))
 				self.packet_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
 				self.packet_history_table.setItem(current_packet_count, 3, QTableWidgetItem(str(crc_ok)))
 				self.packet_history_table.setItem(current_packet_count, 4, QTableWidgetItem(str(csp.priority)))
-				self.packet_history_table.setItem(current_packet_count, 5, QTableWidgetItem(NODES[str(csp.source)]))
+				self.packet_history_table.setItem(current_packet_count, 5, QTableWidgetItem(str(csp.source)))
 				self.packet_history_table.setItem(current_packet_count, 6, QTableWidgetItem(str(csp.destination)))
 				self.packet_history_table.setItem(current_packet_count, 7, QTableWidgetItem(str(csp.dest_port)))
 				self.packet_history_table.setItem(current_packet_count, 8, QTableWidgetItem(str(csp.source_port)))
 				self.packet_history_table.setItem(current_packet_count, 9, QTableWidgetItem(str(csp.flags)))
-
-				self.packet_history_table.item(current_packet_count, 3).setBackground(QtGui.QColor(0, 255, 0))
+				self.packet_history_table.item(current_packet_count, 3).setBackground(QtGui.QColor(255, 0, 0))
 				self.packet_history_table.scrollToBottom()
 
 				self.raw_history_table.insertRow(current_packet_count)
@@ -237,38 +266,10 @@ class Main(QMainWindow):
 				self.raw_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
 				self.raw_history_table.scrollToBottom()
 
-				eventLogger.info('Received SPP over CSP packet of length {LEN} bytes + 4 byte CRC32C check: OK'.format(LEN=csp.getLength()))
-				data = str(csp.getHex()) + str(crc.hex())
-				rawLogger.info('{iso_time},{data}'.format(iso_time=datetime.datetime.utcnow().isoformat(), data=data))
-
-		else:
-			crc_ok = False
-
-			current_packet_count = self.packet_history_table.rowCount()
-
-			self.packet_history_table.insertRow(current_packet_count)
-			self.packet_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
-			self.packet_history_table.setItem(current_packet_count, 1, QTableWidgetItem('--'))
-			self.packet_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
-			self.packet_history_table.setItem(current_packet_count, 3, QTableWidgetItem(str(crc_ok)))
-			self.packet_history_table.setItem(current_packet_count, 4, QTableWidgetItem(str(csp.priority)))
-			self.packet_history_table.setItem(current_packet_count, 5, QTableWidgetItem(str(csp.source)))
-			self.packet_history_table.setItem(current_packet_count, 6, QTableWidgetItem(str(csp.destination)))
-			self.packet_history_table.setItem(current_packet_count, 7, QTableWidgetItem(str(csp.dest_port)))
-			self.packet_history_table.setItem(current_packet_count, 8, QTableWidgetItem(str(csp.source_port)))
-			self.packet_history_table.setItem(current_packet_count, 9, QTableWidgetItem(str(csp.flags)))
-			self.packet_history_table.item(current_packet_count, 3).setBackground(QtGui.QColor(255, 0, 0))
-			self.packet_history_table.scrollToBottom()
-
-			self.raw_history_table.insertRow(current_packet_count)
-			self.raw_history_table.setItem(current_packet_count, 0, QTableWidgetItem(str(datetime.datetime.utcnow())))
-			self.raw_history_table.setItem(current_packet_count, 1, QTableWidgetItem(str(csp.getHex())))
-			self.raw_history_table.setItem(current_packet_count, 2, QTableWidgetItem(str(csp.getLength())))
-			self.raw_history_table.scrollToBottom()
-
-			eventLogger.error('Received CSP packet of length {LEN} bytes - Failed CRC32C!'.format(LEN=str(csp.getLength())))
-			#Don't write to any logs.
-
+				eventLogger.error('Received CSP packet of length {LEN} bytes - Failed CRC32C!'.format(LEN=str(csp.getLength())))
+				#Don't write to any logs.
+		except Exception as e:
+			eventLogger.error('Error occured: {ERR}'.format(ERR=e))
 
 
 
